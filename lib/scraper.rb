eebec208286a7d50359ca2bf2482084b98ecbe97
Bruce Steedman <bruce.steedman@gmail.com>
2015-09-08 16:07:59 +0100

    add most methods, hash merge to do

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,136 +1,81 @@
-# require 'mechanize'
+require 'mechanize'
 
-# class JSONParser < Mechanize::File
-#   attr_reader :json
+class JSONParser < Mechanize::File
+  attr_reader :json
 
-#   def initialize(uri=nil, response=nil, body=nil, code=nil)
-#     super(uri, response, body, code)
-#     @json = JSON.parse(body)
-#   end
-# end
-
-# class IdsDontMatchError < StandardError; end
+  def initialize(uri=nil, response=nil, body=nil, code=nil)
+    super(uri, response, body, code)
+    @json = JSON.parse(body)
+  end
+end
 
 class Scraper
 
-#   Linguistics.use(:en) # for plural method
+  SCHEME = 'http://'
+  DOMAIN = 'gps.digimap.gg/'
+  ROOT = 'arcgis/rest/services/'
 
-  DOMAIN = 'https://gps.digimap.gg/'
-#   URL, KEYS, COLUMNS = '', [], [] # subclasses must overide
+  def initialize(service, layer_num)
+    @service = service
+    @layer_num = layer_num
+    @url = url
+    @agent = Mechanize.new
+    @agent.pluggable_parser['text/plain'] = JSONParser
+    @pk = pk
+    @max = max # maxRecordCount - usually 1000
+    @form = form
+  end
 
-#   attr_reader :form
+  def pk
+    @agent.get(url + '?f=pjson').json['fields'].first['name']
+  end
 
-#   def initialize(lower_id = 0, upper_id = 0)
-#     @lower_id, @upper_id = lower_id, upper_id
-#     @id_array = (@lower_id..@upper_id).to_a
-#     @agent = Mechanize.new
-#     @agent.pluggable_parser['text/plain'] = JSONParser # not 'application/json'..??
-#     @keys = keys
-#     @form = form
-#     @json = json
-#     validate
-#     setup_hash_key_methods
-#     @features = features
-#     @atts = atts
-#     setup_accessor_methods
-#   end
+  def max
+    @agent.get(url + '?f=pjson').json['maxRecordCount'].to_i
+  end
 
-#   def keys
-#     self.class.const_get(:KEYS).zip(self.class.const_get(:COLUMNS)).map { |arr| Hash[*arr] }.inject({}) { |m, e| m.merge(e) }
-#   end
+  def form
+    @agent.get(url + '/query').forms.first
+  end
 
-#   def form
-#     @agent.get(DOMAIN + self.class.const_get(:URL)).forms.first
-#   end
+  def count
+    set_query_params
+    @form.submit(@form.buttons[1]).json['count'].to_i
+  end
 
-#   def json
-#     set_query_params
-#     @form.submit(@form.buttons[1]).json
-#   end
+  def set_query_params(loop_num = nil)
+    @form.fields[0].value = where_text(loop_num)
+    @form.radiobuttons[4].check unless loop_num # count only true
+    @form.fields[6].value = '*'
+    @form.field_with(name: 'f').options[1].select # for JSON
+  end
 
-#   def validate
-#     raise Error unless @form.fields.map(&:name) == FIELDS &&
-#     @form.radiobuttons.map(&:name) == RADIOS
-#   end
+  def data(n)
+    set_query_params(n)
+    @form.submit(@form.buttons[1]).json
+  end
 
-#   def setup_hash_key_methods
-#     (self.class.const_get(:KEYS) + %w(features attributes geometry x y)).each do |key|
-#       Hash.send(:define_method, key.downcase) { self[key] }
-#     end
-#   end
+  def features
+    num_loops.map { |n| data(n)['features'] }
+  end
 
-#   def setup_accessor_methods # meta program accessor methods for all KEY fields
-#     self.class.const_get(:KEYS).map(&:downcase).each do |key|
-#       self.class.send(:define_method, key.en.plural) { @atts.map(&key.to_sym) }
-#     end
-#     # and for geometry - :xs, :ys
-#     self.class.send(:define_method, 'xes') { geometry.map &:x }
-#     self.class.send(:define_method, 'yes') { geometry.map &:y }
-#   end
+  # def all_data
+  #   data[0].merge
+  # end
 
-#   def features
-#     @json['features']
-#   end
+  def url
+    SCHEME + DOMAIN + ROOT + @service + "/MapServer/#{@layer_num}"
+  end
 
-#   def atts
-#     @features.map(&:attributes)
-#   end
+  private
 
-#   def geometry
-#     @features.map(&:geometry)
-#   end
+  def num_loops
+    (count.to_f/@max).ceil
+  end
 
-#   def rings # the shape's polygon rings - or nil for a point
-#     geometry[0]['rings']
-#   end
-
-#   def set_query_params
-#     @form.fields[0].value = query_string # SQL 'WHERE' clause
-#     @form.fields[6].value = self.class.const_get(:KEYS).join(',') # output fields
-#     @form.field_with(name: 'f').options[1].select # for JSON
-#   end
-
-#   def query_string
-#     "OBJECTID >= #{@lower_id} AND OBJECTID <= #{@upper_id}"
-#   end
-
-#   def hash_key(key) # monkey patch Hash for each JSON key required
-#     Hash.send(:define_method, key.downcase) { self[key] }
-#   end
-
-#   def num_records
-#     agent = Mechanize.new
-#     agent.pluggable_parser['text/plain'] = JSONParser
-#     form = agent.get(DOMAIN + self.class.const_get(:URL)).forms.first
-#     form.fields[0].value = "OBJECTID > 0"
-#     form.radiobuttons[4].check # count only true
-#     form.field_with(name: 'f').options[1].select # for JSON
-#     form.submit(form.buttons[1]).json['count'].to_i
-#   end
-
-#   def data
-#     return array_of_hashes.map { |arr| arr.inject &:merge } if rings # otherwise add point coords
-#     coords.zip(array_of_hashes).map(&:flatten).map { |arr| arr.inject &:merge }
-#   end
-
-#   def coords
-#     xes.map { |x| Hash[x: x] }.zip(yes.map { |y| Hash[y: y] })
-#   end
-
-#   def array_of_hashes
-#     self.class.const_get(:KEYS)[1..-1].inject(hashy(self.class.const_get(:KEYS)[0])) do |m, e|
-#       m.zip(hashy e)
-#     end.map &:flatten
-#   end
-
-#   def hashy(key)
-#     self.send(key.downcase.en.plural.to_sym).map do |e|
-#       Hash[@keys[key] => (e ? process(key, e) : nil)]
-#     end
-#   end
-
-#   def process(k, d) # overide depending on processing needs of specific data
-#     return d
-#   end
+  def where_text(n)
+    return "#{pk} > 0" unless n
+    "#{pk} > #{(n -1) * @max} AND #{pk} <= #{n * @max}"
+  end
 
 end
--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -2,9 +2,52 @@ require 'scraper'
 
 describe Scraper do
 
-  context '#new' do
-    it 'instantiated an instance of the class' do
-      expect(Scraper.new.class).to eq Scraper
+  let(:scraper) { Scraper.new('StatesOfJersey/JerseyMappingOL', 0) }
+
+  context '#new(service, layer_num)' do
+    it 'instantiates an instance of the class' do
+      expect(scraper.class).to eq Scraper
+    end
+  end
+
+  context '#new(service, layer_num)' do
+    it 'instantiates an instance of the class' do
+      expect(scraper.url).to eq 'http://gps.digimap.gg/arcgis/rest/services/StatesOfJersey/JerseyMappingOL/MapServer/0'
+    end
+  end
+
+  context '#pk' do
+    it 'returns the "primary key" field for the layer' do
+      expect(scraper.pk).to eq 'OBJECTID'
+    end
+  end
+
+  context '#max' do
+    it 'returns the "maxRecordCount" value for the layer' do
+      expect(scraper.max).to eq 1000
+    end
+  end
+
+  context '#form' do
+    it 'returns a Mechanize::Form for the layer query page' do
+      expect(scraper.form.class).to eq Mechanize::Form
+    end
+  end
+
+   context '#count' do
+    it 'returns the number of records for the layer' do
+      expect(scraper.count).to eq 67537
+    end
+  end
+
+  context '#data(records_set_num)' do
+    it 'returns a hash of json data with no args' do
+      expect(scraper.data(0).class).to eq Hash
+    end
+
+    it 'returns data for the set of records' do
+      puts scraper.data(68).keys
+      expect(scraper.data(68)['features'].count).to eq 537
     end
   end
 
